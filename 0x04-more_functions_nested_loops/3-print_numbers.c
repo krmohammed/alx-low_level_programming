9bbf17dcfcfa73db8e7bbc48d515916cd2374edb
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-03-23 08:26:07 -0700

    printing integers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integers from 0-9
+ *
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}

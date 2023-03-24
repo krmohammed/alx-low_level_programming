f0a25c70b103f11ba3a48b8d15402501a27a8030
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-03-23 18:44:42 -0700

    I am diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,21 @@ void print_diagonal(int n)
 {
 	int j, k;
 
-	for (j = 0; j <= n; j++)
+	if (n <= 0)
 	{
-		for (k = 0; k < j; k++)
-		{
-			if (k < (j - 1))
-			{
-				_putchar(' ');
-				continue;
-			}
-			else
-			{
-				_putchar('\\');
-			}
-		}
 		_putchar('\n');
 	}
-	/*_putchar('\n');*/
+	else
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (k = 0; k < j; k++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+
 }

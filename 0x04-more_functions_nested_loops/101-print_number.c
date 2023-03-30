e2a5ee393cf88c304957f1d2344270d865bef3f3
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-03-30 07:49:55 -0700

    printing numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,13 +13,13 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -i;
 	}
 
 	if (i > 9)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
 
-	_putchar('0' + n % 10);
+	_putchar('0' + i % 10);
 }

feec35d9c6fcb71569d29a5c27766747d6850456
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-03-30 00:47:56 -0700

    capitalizing a sentence

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes a string
+ * @s: string to be capitalized
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char d[] = "\n\t (){};,\"?!";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; d[j] != '\0'; j++)
+		{
+			if (s[i] == d[j] && (s[i + 1] >= 97 && s[i + 1] <= 122))
+			{
+				s[i + 1] -= 32;
+			}
+			if (s[i] == '.' && (s[i + 1] >= 97 && s[i + 1] <= 122))
+			{
+				s[i + 1] -= 32;
+			}
+			if (s[i] == '\t')
+			{
+				s[i] = ' ';
+			}
+		}
+	}
+	return (s);
+}

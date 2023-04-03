fd1262c6c0eaafc750e45f781c07f11d4354a53e
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-03 08:28:55 -0700

    copying from one memory location to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies bytes from memory area to another
+ * @dest: destination memory
+ * @src: source memory
+ * @n: number of bytes
+ *
+ * Return: dest memory
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int add = 0;
+
+	while (n--)
+	{
+		*(dest + add) = *(src + add);;
+		add++;
+	}
+
+	return (dest);
+}

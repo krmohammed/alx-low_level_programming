f67c4a090d4c8cd250ca34117fb9889d4dfccac4
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-06 04:06:30 -0700

    checks for a palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * palindrome_check - checks if a string is the same when reversed
+ * @s: string to be checked
+ * @a: starting index
+ * @z: ending index
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int palindrome_check(char *s, int a, int z)
+{
+	if (a >= z)
+		return (1);
+	else if (s[a]  != s[z])
+		return (0);
+
+	return (palindrome_check(s, a + 1, z - 1));
+}
+
+
+/**
+ * is_palindrome - if a steing is palindrome or not
+ * @s: string to be checked
+ *
+ * Return: 1 if it is, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	if (len <= 1)
+		return (1);
+
+	return (palindrome_check(s, 0, len - 1));
+}

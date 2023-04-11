598fbfa227ec78767bc47d6f3989c14fdd1fd8af
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-11 16:48:20 -0700

    2D arrays

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,21 +11,19 @@
 char *str_concat(char *s1, char *s2)
 {
 	int len1 = 0, len2 = 0, i;
-	char *concat;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = '';
 	if (s2 == NULL)
-		s2 = "";
+		s2 = '';
 
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
 
+	char *concat;
 	concat = malloc(len1 + len2 + 1);
-	if (concat == NULL)
-		return (NULL);
 
 	for (i = 0;  s1[i] != '\0'; i++)
 	{
@@ -36,7 +34,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		concat[len1 + i] = s2[i];
 	}
-	concat[len1 + i] = '\0';
 
 	return (concat);
 }

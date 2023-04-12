7cd6211dbeb069ef508c2a9a57973b8f55126860
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-12 04:17:25 -0700

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,18 +11,18 @@
 char *str_concat(char *s1, char *s2)
 {
 	int len1 = 0, len2 = 0, i;
+	char *concat;
 
 	if (s1 == NULL)
-		s1 = '';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '';
+		s2 = "";
 
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
 
-	char *concat;
 	concat = malloc(len1 + len2 + 1);
 
 	for (i = 0;  s1[i] != '\0'; i++)

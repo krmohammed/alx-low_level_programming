65495949452f08052dd32556baf14978877fd062
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-13 13:34:58 -0700

    contiguous memory alloction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *c_alloc = (char *)malloc(nmemb * size);
+	char *c_alloc = malloc(nmemb * size);
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);

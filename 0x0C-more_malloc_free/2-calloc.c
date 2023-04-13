fde2a9ea4f47ed9da55e67be6a0b6b49d797b187
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-13 06:56:02 -0700

    contigous memory allocation

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: numver of array elements
+ * @size: size of nmemb
+ *
+ * Return: pointer to allocated memory, NULL if it fail
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *c_alloc = (char *)malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	if (c_alloc == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		c_alloc[i] = 0;
+	}
+
+	return (c_alloc);
+}

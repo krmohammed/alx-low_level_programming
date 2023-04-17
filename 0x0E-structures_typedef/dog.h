389867b2ee8266b82e4b264786f2d4ce5fed9983
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-17 15:40:21 -0700

    added a new prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - information about a dog
@@ -18,5 +20,7 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 
 #endif /* DOG_H */

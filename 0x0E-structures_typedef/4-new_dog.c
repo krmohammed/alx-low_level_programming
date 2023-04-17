42706bfd73d3095f42beb6e09b106fb7aef02c53
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-17 15:54:07 -0700

    creating new instance of the dog struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,20 +13,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int len_n, len_o;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	while (name[len_n])
-		len_n++;
-	while (owner[len_o])
-		len_o++;
-
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
 
-	return (dog);
+	return (new_dog);
 }

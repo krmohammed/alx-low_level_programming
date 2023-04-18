902a75685f5faa65209efa087b7f1ae31280f840
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-04-18 08:01:26 -0700

    new dog instance

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,6 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i < name_len; i++)
 		*(name_copy + i) = name[i];
+	*(name_copy + i) = '\0';
 
 
 	owner_copy = malloc(sizeof(char) * (owner_len + 1));
@@ -44,6 +45,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i < owner_len; i++)
 		*(owner_copy + i) = owner[i];
+	*(owner_copy + i) = '\0';
 
 	new_dog->name = name_copy;
 	new_dog->age = age;

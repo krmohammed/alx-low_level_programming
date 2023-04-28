be4baa2ed8bed43baaf3440ae0243d652d916a83
mohrahman <mohrahman558@gmail.com>
2023-04-28 02:30:03 +0000

    inserts a node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		ptr = *head;
-		while(ptr->next != NULL)
+		while (ptr->next != NULL)
 			ptr = ptr->next;
 		ptr->next = tempo;
 	}

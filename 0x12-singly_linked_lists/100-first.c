196e237bc63753d3a91a3c32805acff7e1043ffc
mohrahman <mohrahman558@gmail.com>
2023-04-28 03:53:00 +0000

    executes before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * before_main - executes before the main function
+ *
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

2e21a29849a345d743581323df5e70ab4b6cdc64
mohrahman <mohrahman558@gmail.com>
2023-04-28 04:11:20 +0000

    my first assembly language code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+	message db "Hello, Holberton", 0
+	format db "%s\n", 0
+
+section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rbp, rsp
+
+	push qword message
+	push qword format
+	call printf
+
+	add rsp, 16
+	mov rsp, rbp
+	pop rbp
+	ret

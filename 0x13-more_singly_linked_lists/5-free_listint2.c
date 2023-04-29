812fd6349200a18de5ff727ec5672a1cc1066afc
mohrahman <mohrahman558@gmail.com>
2023-04-29 22:01:17 +0000

    frees a linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: linked list
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	while (*head != NULL)
+	{
+		p = *head;
+		*head = (*head)->next;
+		free(p);
+	}
+
+	*head = NULL;
+	head = NULL;
+
+}

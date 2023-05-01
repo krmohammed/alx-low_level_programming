d7bfd52d50b5c2add85c3d1a1bc01c4587dfe3a3
mohrahman <mohrahman558@gmail.com>
2023-05-01 17:31:44 +0000

    inserting node at nth position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a node at nth position
+ * @head: singly linked list
+ * @idx: nth position
+ * @n: node data
+ *
+ * Return: address of new node, NULL if it fails
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *p1, *p2;
+	unsigned int k;
+
+	if (*head == NULL)
+		return (NULL);
+
+	p2 = malloc(sizeof(listint_t));
+	if (p2 == NULL)
+		return (NULL);
+
+	p2->n = n;
+	p2->next = NULL;
+
+	p1 = *head;
+
+	for (k = 0; p1 != NULL; k++)
+	{
+		if (k == idx - 1)
+		{
+			p2->next = p1->next;
+			p1->next = p2;
+
+			return(p2);
+		}
+
+		p1 = p1->next;
+	}
+
+	return (NULL);
+}
+

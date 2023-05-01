e024b2856d514e39ac06220c6910d825908c9267
mohrahman <mohrahman558@gmail.com>
2023-05-01 02:57:04 +0000

    simplifying things

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *p, *temp;
+	listint_t *p;
 	unsigned int i;
 
 	if (head == NULL)
@@ -22,8 +22,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (i == index)
 		{
-			temp = p;
-			return (temp);
+			return (p);
 		}
 		p = p->next;
 	}

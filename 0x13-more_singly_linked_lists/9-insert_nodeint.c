9c939610873b38f4de63c5f2c4cf6fbb92693414
mohrahman <mohrahman558@gmail.com>
2023-05-02 02:05:17 +0000

    inserting node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *p1, *p2;
 	unsigned int k;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
+		return (NULL);
+	
+	p1 = *head;
+	for (k = 0; k < idx - 1 && p1 != NULL; k++)
+		p1 = p1->next;
+
+	if (p1 == NULL)
 		return (NULL);
 
 	p2 = malloc(sizeof(listint_t));
@@ -22,21 +29,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	p2->n = n;
-	p2->next = NULL;
 
-	p1 = *head;
-
-	for (k = 0; p1 != NULL; k++)
-	{
-		if (k == idx - 1)
-		{
-			p2->next = p1->next;
-			p1->next = p2;
-			return (p1);
-		}
-			p1 = p1->next;
-	}
-
-	return (NULL);
+	p2->next = p1->next;
+	p1->next = p2;
+	
+	return (p2);
 }
 

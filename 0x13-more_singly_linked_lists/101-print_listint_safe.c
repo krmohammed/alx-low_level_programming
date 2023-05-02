96f8acb6670d48a7be7b70cce4b9e36717d542a6
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-05-02 11:28:46 -0700

    print in safe mode

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list in safe mode
+ * @head: linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	int nodes = 0;
+
+	if (head == NULL)
+		exit (98);
+
+	fast = head;
+	while (fast != NULL)
+	{
+		nodes++;
+		printf("[%p] %d\n", (void *)fast, fast->n);
+		slow = fast;
+		fast = fast->next;
+		
+		if (fast >= slow)
+			{
+			printf("-> [%p] %d\n", (void *)fast, fast->n);
+			exit(98);
+		}
+	}
+
+	return (nodes);
+}

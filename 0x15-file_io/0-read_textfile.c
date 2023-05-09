690c952cebef5d61df8b2884c5a57be137638ab4
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-05-08 17:20:22 -0700

    fixing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(char) * letters + 1);
 	if (buff == NULL)
 		return (0);
 

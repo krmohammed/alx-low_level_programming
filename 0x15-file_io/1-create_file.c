b4339427a576a970ec7b067f15af1d3e2d07ed92
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-05-08 18:03:44 -0700

    create a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,12 +20,15 @@ int create_file(const char *filename, char *text_content)
 	for (str_len = 0; text_content[str_len] != 0; str_len++)
 		;
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	n_write = write(fd, text_content, str_len);
 	if (n_write == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	close(fd);
 

4665e42d6d2bd5f64ad1ff3f4c70c34c07b5fc70
mohrahman <mohrahman558@gmail.com>
2023-06-09 16:19:34 +0000

    number of cmd arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of cmd line args
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (Always)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}

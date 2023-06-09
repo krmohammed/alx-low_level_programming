509c22a109e9a5a06b6152ded061a2be304c6229
mohrahman <mohrahman558@gmail.com>
2023-06-09 16:51:51 +0000

    sum of cmd args (integers only)

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive integers from the cmd line
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (Always)
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y, add = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (x = 1; argv[x] != NULL; x++)
+	{
+		y = 0;
+		while(argv[x][y])
+		{
+			if (argv[x][y] < '0' || argv[x][y] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			y++;
+		}
+		add += atoi(argv[x]);
+	}
+
+	printf("%d\n", add);
+
+	return (0);
+}

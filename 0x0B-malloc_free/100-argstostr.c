6051181f089a6ee3b23cab8da7dbd850f7af643f
mohrahman <mohrahman558@gmail.com>
2023-06-13 17:01:48 +0000

    malloc fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,12 +25,12 @@ char *argstostr(int ac, char **av)
 		av_len++;
 	}
 
-	new = malloc(sizeof(char) * av_len);
+	new = malloc((sizeof(char) * av_len) + 1);
 	if (new == NULL)
 		return (NULL);
 
 	k = 0;
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{

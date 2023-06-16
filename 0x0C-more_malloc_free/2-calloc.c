28390cba389389b36c5d5e4b9ab5886716f8b237
mohrahman <mohrahman558@gmail.com>
2023-06-16 00:32:28 +0000

    documenting return type

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (new_str == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		new_str[i] = 0;
 
 	return (new_str);

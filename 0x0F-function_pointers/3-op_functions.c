21a810c5da8cdc407a770ef514bfcb323a2cfd1c
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2023-06-21 13:59:11 -0700

    betty fixes

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -23,7 +23,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 
 
@@ -37,7 +37,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
 
 
@@ -51,7 +51,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-        return (a / b);
+	return (a / b);
 }
 
 
@@ -65,5 +65,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-        return (a % b);
+	return (a % b);
 }

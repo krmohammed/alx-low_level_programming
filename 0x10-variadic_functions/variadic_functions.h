c695b983912c60ecfbaf319a2f9d2c67f9e88f5d
mohrahman <mohrahman558@gmail.com>
2023-06-22 12:15:02 +0000

    new library added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VAR_FUNC
 #define VAR_FUNC
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

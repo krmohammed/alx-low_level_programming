0adff57df1c51c905826372dfc912f7fc219ba1b
mohrahman <mohrahman558@gmail.com>
2023-10-13 07:57:10 +0000

    efficiency

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,46 +12,37 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = index;
-	dlistint_t *ptr1 = *head, *ptr2;
+	dlistint_t *ptr1 = *head;
 
 	if ((head == NULL) | (*head == NULL))
 		return (-1);
-	if (index == 0)
+	if (index == 0 && ptr1->next == NULL)
 	{
-		if (ptr1->next == NULL)
-		{
-			*head = NULL;
-			free(*head);
-		}
-		else
-		{
-			*head = ptr1->next;
-			/*ptr1->next = NULL;*/
-			free(ptr1);
-			(*head)->prev = NULL;
-		}
+		*head = NULL;
+		free(ptr1);
 		return (1);
 	}
-	while (i > 0)
+	if (index == 0)
 	{
-		ptr1 = ptr1->next;
-		i--;
+		*head = ptr1->next;
+		free(ptr1);
+		(*head)->prev = NULL;
+		return (1);
 	}
+	for (; i > 0 && ptr1 != NULL; i--)
+		ptr1 = ptr1->next;
+	if (i > 0)
+		return (-1);
 	if (ptr1->next == NULL)
 	{
-		ptr2 = ptr1->prev;
-		ptr2->next = NULL;
-		/*ptr1->prev = NULL;*/
+		ptr1->prev->next = NULL;
 		free(ptr1);
 		return (1);
 	}
 	else
 	{
-		ptr2 = ptr1->prev;
-		ptr2->next = ptr1->next;
-		ptr1->next->prev = ptr2;
-		/*ptr1->next = NULL;
-		ptr1->prev = NULL;*/
+		ptr1->prev->next = ptr1->next;
+		ptr1->next->prev = ptr1->prev;
 		free(ptr1);
 		return (1);
 	}

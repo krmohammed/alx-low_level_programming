d890e072de880770ee21739b5cbb9da400f35026
mohrahman <mohrahman558@gmail.com>
2023-10-13 00:53:53 +0000

    betty fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	dlistint_t *ptr1 = *head, *ptr2, *ptr3;
-	
+
 	if ((head == NULL) | (*head == NULL))
 		return (-1);
 	while (ptr1 != NULL)
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	if ((ptr1->prev == NULL) && (ptr1->next == NULL))
 	{
-		*head = NULL;
+		head = NULL;
 		return (1);
 	}
 	if (ptr1->prev == NULL)

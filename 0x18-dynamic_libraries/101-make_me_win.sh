943988024e5e11a7043868fa8af4021ee4555c8f
mohrahman <mohrahman558@gmail.com>
2023-10-17 03:30:53 +0000

    sysadmin hack

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /temp /
+wget -P /temp https://github.com/krmohammed/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libjackpot.so
+export LD_PRELOAD=/temp/libjacpot.so

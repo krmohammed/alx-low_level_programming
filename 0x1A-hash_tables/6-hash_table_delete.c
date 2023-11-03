32c148ff6201472598dd40b8664080cb94f8c759
mohrahman <mohrahman558@gmail.com>
2023-11-03 10:30:06 +0000

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,19 +9,22 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *ptr;
+	hash_node_t *ptr1, *ptr2;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
-			ptr = ht->array[i];
-			while (ptr)
+			ptr1 = ht->array[i];
+			while (ptr1)
 			{
-				free(ptr->key);
-				free(ptr->value);
-				free(ptr);
-				ptr = ptr->next;
+				ptr2 = ptr1;
+				if (ptr1->key)
+					free(ptr1->key);
+				if (ptr1->value)
+					free(ptr1->value);
+				free(ptr2);
+				ptr1 = ptr1->next;
 			}
 		}
 	}

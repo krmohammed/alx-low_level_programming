7dd9b4638a48f5873a99d63d409031f6ea44ba40
mohrahman <mohrahman558@gmail.com>
2023-11-03 01:29:52 +0000

    betty fix

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,7 +12,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash_code = 5381;
 	int k;
-	
+
 	while ((k = *str++))
 		hash_code = ((hash_code << 5) + hash_code) + k;
 

3a98a05f53c29699e4e24035b694327bf7eeb1f5
mohrahman <mohrahman558@gmail.com>
2023-11-03 09:50:06 +0000

    deleting table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *ptr;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			ptr = ht->array[i];
+			while (ptr)
+			{
+				free(ptr->key);
+				free(ptr->value);
+				free(ptr);
+				ptr = ptr->next;
+			}
+		}
+	}
+}

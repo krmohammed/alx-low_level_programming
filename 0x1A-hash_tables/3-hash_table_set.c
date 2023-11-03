7b494ae8ba3a45eecfff26be6ab33d619da9ba55
mohrahman <mohrahman558@gmail.com>
2023-11-03 07:03:53 +0000

    ht adder

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,16 +13,35 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *hn;
-	
+
 	if (key == NULL)
 		return (0);
 	hn = malloc(sizeof(hash_node_t));
 	if (hn == NULL)
 		return (0);
-	hn->key = (char *)key;
+	hn->key = strdup(key);
+	if (hn->key == NULL)
+	{
+		free(hn);
+		return (0);
+	}
 	hn->value = strdup(value);
+	if (hn->value == NULL)
+	{
+		free(hn->key);
+		free(hn);
+		return (0);
+	}
 
 	index = key_index((const unsigned char *)key, ht->size);
+	
+	if (ht->array[index])
+	{
+		hn->next = ht->array[index];
+		ht->array[index] = hn;
+	}
+	else
+		ht->array[index] = hn;
 
 	return (1);
 }

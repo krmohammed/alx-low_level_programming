2160a0a9fd090ee1e8fe938f53c3ffedf2c4e1f3
mohrahman <mohrahman558@gmail.com>
2023-12-21 08:52:15 +0000

    technical interview prep

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,3 +11,25 @@ def island_perimeter(grid):
         grid (list): list of lists of integers, rectangular
         equal row lengths
     """
+    if grid is None:
+        return
+
+    g_len = len(grid)
+    row_len = len(grid[0])
+    sum = 0
+
+    for i in range(g_len):
+        for j in range(row_len):
+            if grid[i][j] == 1:
+                sum += 4
+
+                if i != 0 and grid[i - 1][j] == 1:
+                    sum -= 1
+                if i != g_len - 1 and grid[i + 1][j] == 1:
+                    sum -= 1
+
+                if j != 0 and grid[i][j - 1] == 1:
+                    sum -= 1
+                if j != row_len - 1 and grid[i][j + 1] == 1:
+                    sum -= 1
+    return sum

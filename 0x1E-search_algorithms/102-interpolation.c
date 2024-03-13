23b1ad67d9f6e64eadcea78387da27d6ea5d6fc7
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-03-13 12:12:39 +0000

    interpolation search algo

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -19,7 +19,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (low < high)
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low]))* (value - array[low]));
+		pos = low + (((double)(high - low) / (array[high] - array[low])) *\
+				(value - array[low]));
 
 		if (pos <= size - 1)
 			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
